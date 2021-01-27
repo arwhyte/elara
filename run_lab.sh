433cc8b16d458a0ca36221d39241f628561ecd0a
Anthony Whyte <anthwhyte@gmail.com>
2021-01-27 12:16:54 -0500

    Tidy up

--- run_lab.sh
+++ run_lab.sh
@@ -3,10 +3,11 @@
 # User-supplied values
 PORT=$1
 VOLUME=$2
+NAME=$3
 
 docker run \
   --rm \
   -p ${PORT}:8888 \
   -v ${VOLUME}:/home/jovyan/work \
   -e JUPYTER_ENABLE_LAB=yes \
-  --name lab elara
+  --name ${NAME} elara
